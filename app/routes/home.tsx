baaadcc05945b3dfa3e6710c3e32ebaa452dbe23
Coding <codingmaniac227@gmail.com>
2025-09-09 08:00:29 -0400

    feat(home): scaffold Home route with hero heading and description - Add backgroujnd image and main section - Add h1 and h2

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,23 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "myParser" },
+    { name: "description", content: "Tailor to your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className='main-section'>
+      <div className='page-heading'>
+        <h1>Tailor Your Resume in Seconds</h1>
+        <h2>Analyze your resume against any job description and get instant, AI-driven tips to stand out.</h2>
+      </div>
+    </section>
+  </main>;
 }
