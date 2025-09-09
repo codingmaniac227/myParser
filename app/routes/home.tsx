4959115eec6cbebb72fe6a455df72701b88cfdec
Coding <codingmaniac227@gmail.com>
2025-09-09 09:42:34 -0400

    feat(home): render resume cards with overall score preview

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,19 +16,19 @@ export default function Home() {
     <Navbar />
 
     <section className='main-section'>
-      <div className='page-heading'>
-        <h1>Tailor Your Resume in Seconds</h1>
-        <h2>Analyze your resume against any job description and get instant, AI-driven tips to stand out.</h2>
-      </div>
-    </section>
-
-    {resumes.length > 0 && (
-        <div className="resumes-section">
-          {resumes.map(resume => (
-              <ResumeCard key={resume.id} resume={resume}></ResumeCard>
-          ))}
+        <div className='page-heading'>
+          <h1>Tailor Your Resume in Seconds</h1>
+          <h2>Analyze your resume against any job description and get instant, AI-driven tips to stand out.</h2>
         </div>
-    )}
 
+
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map(resume => (
+                <ResumeCard key={resume.id} resume={resume}></ResumeCard>
+            ))}
+          </div>
+      )}
+    </section>
   </main>;
 }
